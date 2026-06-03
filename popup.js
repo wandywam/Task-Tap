fcb17f33719d04d2b671a77fa9753abc13d4ca25
Randy Lam <randylam1010@gmail.com>
2026-06-02 18:39:53 -0700

    Added Google Auth setup.

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ const entryForm = document.getElementById("entryForm");
 const statusMessage = document.getElementById("statusMessage");
 
 const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
 const authStatus = document.getElementById("authStatus");
 
 // Autofill today's date
@@ -18,7 +19,9 @@ now.setMinutes(now.getMinutes());
 const defaultTime = now.toTimeString().slice(0, 5);
 document.getElementById("timeInput").value = defaultTime;
 
+// Config vars
 let currentMode = "task";
+let isSignedIn = false;
 
 function switchToTask() {
     currentMode = "task";
@@ -28,7 +31,7 @@ function switchToTask() {
     createBtn.textContent = "Create Task";
     statusMessage.textContent = "";
 
-    chrome.local.storage.set({mode: "task"});
+    chrome.storage.local.set({mode: "task"});
 }
 
 function switchToEvent() {
@@ -39,11 +42,58 @@ function switchToEvent() {
     createBtn.textContent = "Create Event";
     statusMessage.textContent = "";
 
-    chrome.local.storage.set({mode: "event"});
+    chrome.storage.local.set({mode: "event"});
 }
 
+// Check if token stored and user signed in --> keep user signed in
+chrome.storage.local.get(
+    ["accessToken", "signedIn"],
+    (result) => {
+        if (result.signedIn && result.accessToken) {
+            isSignedIn = true;
+
+            authStatus.textContent = "Signed in";
+            signInBtn.classList.add("hidden");
+            signOutBtn.classList.remove("hidden");
+        }
+    }
+);
+
+// Sign in/out buttons
 signInBtn.addEventListener("click", () => {
-    authStatus.textContent = "Sign in coming soon...";
+    chrome.identity.getAuthToken({ interactive: true}, (token) => {
+        if (chrome.runtime.lastError || !token) {
+            authStatus.textContent = "Sign in failed";
+            console.log(chrome.runtime.lastError);
+            return;
+        }
+    
+    isSignedIn = true;
+    authStatus.textContent = "Signed in";
+    signInBtn.classList.add("hidden");
+    signOutBtn.classList.remove("hidden");
+
+    console.log("Access token: ", token);
+
+    // store token = user signed in
+    chrome.storage.local.set({
+        accessToken: token,
+        signedIn: true
+        });
+    });
+});
+signOutBtn.addEventListener("click", () => {
+    isSignedIn = false;
+
+    // remove stored sign in token
+    chrome.storage.local.remove([
+        "accessToken",
+        "signedIn"
+    ]);
+
+    authStatus.textContent = "Not signed in";
+    signInBtn.classList.remove("hidden");
+    signOutBtn.classList.add("hidden");
 })
 
 // When Task Button toggle clicked
@@ -65,13 +115,18 @@ chrome.storage.local.get(["mode"], (result) => {
 entryForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    if (!isSignedIn) {
+        statusMessage.textContent = "Please sign in first.";
+        return;
+    }
+
     const title = document.getElementById("titleInput").value.trim();
     if (!title) {
         statusMessage.textContent = "Please enter a valid title.";
         return;
     }
 
-    // Create Task/Event object
+    // create Task/Event object
     const data = {
         type: currentMode,
         title: title,
@@ -87,6 +142,7 @@ entryForm.addEventListener("submit", (event) => {
     console.log(data);
     statusMessage.textContent = `${currentMode === "task" ? "Task" : "Event"} data logged.`;
 
+    // clear input boxes
     document.getElementById("titleInput").value = "";
     document.getElementById("notesInput").value = "";
 
